b2476805f4ca3fb61cd9c03796f5658f31007ba7
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-10-16 20:43:37 +0530

    format

--- 01/info_input.c
+++ 01/info_input.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-int main()
-{
-    char name[50], email[50], adress[200], phone[13];
-    int age;
+int main() {
+  char name[50], email[50], adress[200], phone[13];
+  int age;
 
-    // Taking user input
-    printf("Enter Name : ");
-    fgets(name, sizeof(name), stdin);
-    printf("Enter Age : ");
-    scanf("%d", &age);
-    getchar(); // reads the \n to allow email(string) input
-    printf("Enter Email : ");
-    fgets(email, sizeof(email), stdin);
-    printf("Enter Address : ");
-    fgets(adress, sizeof(adress), stdin);
-    printf("Enter Phone : ");
-    fgets(phone, sizeof(phone), stdin);
+  // Taking user input
+  printf("Enter Name : ");
+  fgets(name, sizeof(name), stdin);
+  printf("Enter Age : ");
+  scanf("%d", &age);
+  getchar(); // reads the \n to allow email(string) input
+  printf("Enter Email : ");
+  fgets(email, sizeof(email), stdin);
+  printf("Enter Address : ");
+  fgets(adress, sizeof(adress), stdin);
+  printf("Enter Phone : ");
+  fgets(phone, sizeof(phone), stdin);
 
-    // Displaying entered information
-    printf("\n\nName : %s", name);
-    printf("Age : %d\n", age);
-    printf("Email : %s", email);
-    printf("Address : %s", adress);
-    printf("Phone : %s", phone);
+  // Displaying entered information
+  printf("\n\nName : %s", name);
+  printf("Age : %d\n", age);
+  printf("Email : %s", email);
+  printf("Address : %s", adress);
+  printf("Phone : %s", phone);
 
-    return 0;
+  return 0;
 }
