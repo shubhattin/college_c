57d16f6f4b3d47ef25665d7ce98239d8fb24a2f7
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-03-23 21:45:54 +0530

    fix formar nvim

--- 03/AddMatrix.c
+++ 03/AddMatrix.c
@@ -1,48 +1,39 @@
 #include <stdio.h>
 
-int main()
-{
-    int A[3][3], B[3][3], C[3][3];
-    int i, j;
+int main() {
+  int A[3][3], B[3][3], C[3][3];
+  int i, j;
 
-    printf("Enter Matrix 1:\n");
-    for (i = 0; i < 3; i++)
-    {
-        printf("Enter row %d: ", i + 1);
-        for (j = 0; j < 3; j++)
-        {
-            scanf("%d", &A[i][j]);
-        }
+  printf("Enter Matrix 1:\n");
+  for (i = 0; i < 3; i++) {
+    printf("Enter row %d: ", i + 1);
+    for (j = 0; j < 3; j++) {
+      scanf("%d", &A[i][j]);
     }
+  }
 
-    printf("Enter Matrix 2:\n");
-    for (i = 0; i < 3; i++)
-    {
-        printf("Enter row %d: ", i + 1);
-        for (j = 0; j < 3; j++)
-        {
-            scanf("%d", &B[i][j]);
-        }
+  printf("Enter Matrix 2:\n");
+  for (i = 0; i < 3; i++) {
+    printf("Enter row %d: ", i + 1);
+    for (j = 0; j < 3; j++) {
+      scanf("%d", &B[i][j]);
     }
+  }
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            C[i][j] = A[i][j] + B[i][j];
-        }
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      C[i][j] = A[i][j] + B[i][j];
     }
+  }
 
-    /* Print the result */
-    printf("\nSum of Matrices :\n");
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            printf("%d\t", C[i][j]);
-        }
-        printf("\n");
+  /* Print the result */
+  printf("\nSum of Matrices :\n");
+  for (i = 0; i < 3; i++) {
+    for (j = 0; j < 3; j++) {
+      printf("%d\t", C[i][j]);
     }
+    printf("\n");
+  }
 
-    return 0;
+  return 0;
 }
--- 03/ArrayAdress.c
+++ 03/ArrayAdress.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 
-int main()
-{
-    int A[5] = {1, 2, 3, 4, 5};
-    int i, j;
+int main() {
+  int A[5] = {1, 2, 3, 4, 5};
+  int i, j;
 
-    // Printing the array
-    printf("Array A\n");
-    for (i = 0; i < 5; i++)
-        printf("%d, ", A[i]);
-    printf("\n");
+  // Printing the array
+  printf("Array A\n");
+  for (i = 0; i < 5; i++)
+    printf("%d, ", A[i]);
+  printf("\n");
 
-    int index=3;
-
-    void * base = (void *)A;
+  int index = -1;
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  for (i = 0; i < 5; i++) {
+    if (A[i] == num) {
+      index = i;
+      break;
+    }
+  }
+  if (index != -1) {
+    void *base = (void *)A;
     printf("Base address of array A is %p\n", base);
-    void * address = (void *)&A[index];
-    printf("Base address of array A is %p\n", address);
-    printf("Difference in Adress is %p\n",  address - base);
-
-    return 0;
+    void *address = (void *)&A[index];
+    printf("Address of array A is %p\n", address);
+    printf("Difference in Adress is %p\n", address - base);
+  } else {
+    printf("Number Not found\n");
+  }
+  return 0;
 }
