7e2686ebc5d8a95e6c1ce541c909382c2eb7cbbe
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-04-23 11:57:12 +0530

    Done DOuble Linked List

--- 04/DoubleLink.c
+++ 04/DoubleLink.c
@@ -50,6 +50,39 @@ void insert_middle(Node *head, int data, int index) {
   printf("\tIndex %d not found in Linked List\n", index);
 }
 
+void delete_head(Node **head) {
+  Node *new_head = (*head)->next;
+  new_head->prev = NULL;
+  free(*head);
+  *head = new_head;
+}
+
+void delete_end(Node *head) {
+  Node *next = head;
+  while (next->next != NULL) {
+    next = next->next;
+  }
+  (next->prev)->next = NULL;
+  free(next);
+}
+
+void delete_middle(Node *head, int index) {
+  int i = 0;
+  Node *next = head;
+  Node *tmp = NULL;
+  while (next != NULL) {
+    if (++i == index && next->next != NULL) {
+      tmp = next->next;
+      next->next = tmp->next;
+      tmp->prev = next;
+      free(tmp);
+      return;
+    }
+    next = next->next;
+  }
+  printf("\tIndex %d not found in Linked List\n", index);
+}
+
 void print_link_list(Node *head) {
   Node *next = head;
   while (next != NULL) {
@@ -68,9 +101,20 @@ int main() {
   insert_end(head, 5);
   insert_end(head, 6);
   insert_middle(head, 4, 2);
+  // after index 2 (after 3rd element)
 
   printf("Linked List after Insertion: \n");
   print_link_list(head);
   printf("\n");
+
+  // Deletion
+  delete_head(&head);
+  delete_end(head);
+  delete_middle(head, 2);
+
+  printf("Linked List after Deletion: \n");
+  print_link_list(head);
+  printf("\n");
+
   return 0;
 }
