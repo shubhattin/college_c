6bac257468cd60cde9f6b2b26182dc09333fcf5e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-05-04 08:02:17 +0530

    Circular Done

--- 04/CircularLink.c
+++ 04/CircularLink.c
@@ -51,6 +51,43 @@ void insert_middle(Node *head, int data, int index) {
   printf("\tIndex %d not found in Linked List\n", index);
 }
 
+void delete_head(Node **head) {
+  Node *last = (*head)->next;
+  Node *new_head = (*head)->next;
+  while (last->next != *head) {
+    last = last->next;
+  }
+  last->next = new_head;
+  free(*head);
+  *head = new_head;
+}
+
+void delete_end(Node *head) {
+  Node *last = head->next;
+  Node *prev = NULL;
+  while (last->next != head) {
+    prev = last;
+    last = last->next;
+  };
+  free(last);
+  prev->next = head;
+}
+
+void delete_middle(Node *head, int index) {
+  int i = 0;
+  Node *node = head;
+  Node *tmp = NULL;
+  while (node->next != NULL) {
+    if (++i == index && (node->next != NULL || node->next == head)) {
+      tmp = node->next;
+      node->next = tmp->next;
+      free(tmp);
+      return;
+    }
+    node = node->next;
+  }
+}
+
 void print_lnk_list(Node *head) {
   Node *next = head;
   do {
@@ -65,11 +102,11 @@ int main() {
   // inserting elements
   insert_head(&head, 21);
   insert_head(&head, 20);
-  insert_head(&head, 1);
+  insert_head(&head, 19);
   insert_end(head, 2);
   insert_end(head, 3);
   insert_middle(head, 5, 1);
-  insert_middle(head, 9, 6);
+  insert_middle(head, 90, 6);
   printf("Linked List Elements After Insertion: \n");
   print_lnk_list(head);
   printf("\n");
@@ -77,6 +114,9 @@ int main() {
   // This inserts the Node after Index 3 (after 4th element)
 
   // deleting nodes
+  delete_head(&head);
+  delete_end(head);
+  delete_middle(head, 3);
 
   printf("Linked List Elements After Deletion: \n");
   print_lnk_list(head);
